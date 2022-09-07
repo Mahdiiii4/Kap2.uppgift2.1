1c0ffaf221e9d83cd530c0f2c9ee88502f7cb5b3
Mahdiiii4 <111962604+Mahdiiii4@users.noreply.github.com>
2022-09-07 12:12:27 +0200

    Kap2.uppgift2.10
    C# basis

--- Kap2.uppgift2.1/Program.cs
+++ Kap2.uppgift2.1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.WriteLine("vilken stad du komemr från");
+                string stad = Console.ReadLine();
+                Console.WriteLine("Vilken är den dromsresmål");
+                string resmål = Console.ReadLine();
+            
+                Console.WriteLine("Hej " +stad+ "s bo. Jag hoppas att du får resa till " +resmål+ ".");
+            }
+    }
+}
