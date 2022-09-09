64cf26a676bab89cd8a22b6b52a8a4212bcfb8e7
E-Svensson <89245202+Pingvu@users.noreply.github.com>
2022-09-09 10:31:11 +0200

    uppgift 2.10

--- Kap2.uppgift2.1/Program.cs
+++ Kap2.uppgift2.1/Program.cs
@@ -5,12 +5,10 @@ namespace Uppgift_2._1
     {
         static void Main(string[] args)
             {
-                Console.WriteLine("vilken stad du komemr från");
-                string stad = Console.ReadLine();
-                Console.WriteLine("Vilken är den dromsresmål");
-                string resmål = Console.ReadLine();
-            
-                Console.WriteLine("Hej " +stad+ "s bo. Jag hoppas att du får resa till " +resmål+ ".");
+                Console.WriteLine("vilken är den favorit bok?");
+                string bok = Console.ReadLine();
+
+                Console.WriteLine("Den favorit bok är " +bok+ "");
             }
     }
 }
